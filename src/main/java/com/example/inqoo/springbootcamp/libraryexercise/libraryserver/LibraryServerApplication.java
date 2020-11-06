73500d8e20ae05bc62abe5dc8905a8e81c98c73b
Przemek <przemyslawdopart@gmail.com>
2020-11-06 17:48:57 +0100

    first

---
+++ src/main/java/com/example/inqoo/springbootcamp/libraryexercise/libraryserver/LibraryServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.inqoo.springbootcamp.libraryexercise.libraryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServerApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/inqoo/springbootcamp/libraryexercise/libraryserver/LibraryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inqoo.springbootcamp.libraryexercise.libraryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
